e62ea494ecfbe97d8cca7dd62aa9f5556a66cb7b
juliabeatriz09 <159266648+juliabeatriz09@users.noreply.github.com>
2024-10-27 12:12:44 -0300

    Add files via upload

---
+++ scriptcad.js
@@ -0,0 +1,9 @@
+function process(quant){
+    var value = parseInt(document.getElementById("quant").value);
+    value+=quant;
+    if(value < 1){
+      document.getElementById("quant").value = 1;
+    }else{
+    document.getElementById("quant").value = value;
+    }
+  }
